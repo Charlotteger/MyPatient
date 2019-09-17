a55928bbac38becbfd880bc83873ba6073dc5a68
thibaut <thibaut.raven@gmail.com>
2019-09-17 10:56:06 +0200

    ajout bouton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,14 @@ import { Patient } from 'src/app/classes/Patient';
 
 export class AppComponent {
   title = 'my-patient';
-  constructor(private http: HttpClient) {}
-
+  //constructor(private http: HttpClient) {}
+  selecteur:number =0;
+  button1():void{
+    this.selecteur=1
+  }
+  button2():void{
+    this.selecteur=2
+  }
   patienttest:Patient = {
     identification:536,
     name:"Claire Estibal",
