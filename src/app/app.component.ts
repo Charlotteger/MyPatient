87c45cfbab53e99d02f6452083090fbf4c026999
GERBER Charlotte <Charlotte.GERBER@berger-levrault.com>
2019-09-17 11:51:50 +0200

    get patient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,8 @@ import { Patient } from 'src/app/classes/Patient';
 
 export class AppComponent {
   title = 'my-patient';
-  constructor(private http: HttpClient) {}
-
-  patienttest:Patient = {
+  server = "https://fhir.eole-consulting.io/api/";
+  patientTest:Patient = {
     identification:536,
     name:"Claire Estibal",
     telephone:"0643197104",
@@ -20,4 +19,23 @@ export class AppComponent {
     naissance:"le 3 janvier",
     adresse:"endroit random riviere"
   };
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getPatient().then(result => {
+      console.log(result);  
+      for(var prop in result){
+        console.log(prop);
+      } 
+    })
+  }
+
+  private getPatient(): Promise<any> {
+    return this.http.get(this.server + "patient/5d80aa2832364000151f8ad3").toPromise().catch(this.handleError);
+  }
+
+  private handleError(error : any): Promise<any> {
+    console.error('An error occured', error);
+    return Promise.reject(error.message || error);
+  }
 }
